1d9384906ccc7ba9c6aed9bf5a35c846cf26d2ae
hhjoo <joohhcse@gmail.com>
2024-05-23 11:23:01 +0900

    Init

---
+++ android/app/src/main/kotlin/com/jooplayconsole/stock_bible/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jooplayconsole.stock_bible
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Text('stock_bible project init'),
+      ),
+    ),
+  );
+}
